43fedd273cd7b7cbce40b1c3145ba0fe08741e56
Joseph Mirabel <jmirabel@laas.fr>
2018-03-21 16:36:12 +0100

    Add home position.

--- src/hpp/corbaserver/manipulation/tiago/robot.py
+++ src/hpp/corbaserver/manipulation/tiago/robot.py
@@ -8,3 +8,20 @@ class Robot (_Parent):
 
     def __init__ (self, compositeName, name, rootjointtype, load = True):
         super(Robot, self).__init__(compositeName, name, rootjointtype, load)
+        self.tiagoName = name
+
+    def homePosition(self,q=None):
+        if q is None:
+            q = self.getCurrentConfig()
+        from math import pi
+        q[self.rankInConfiguration[self.tiagoName + "/torso_lift_joint"]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/arm_1_joint"]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/arm_2_joint"]] = - pi / 2
+        q[self.rankInConfiguration[self.tiagoName + "/arm_3_joint"]] = - pi / 2
+        q[self.rankInConfiguration[self.tiagoName + "/arm_4_joint"]] = 2.35619449019
+        q[self.rankInConfiguration[self.tiagoName + "/arm_5_joint"]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/arm_6_joint"]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/arm_7_joint"]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/head_1_joint" ]] = 0
+        q[self.rankInConfiguration[self.tiagoName + "/head_2_joint"]] = 0
+        return q
